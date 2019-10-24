161aba666c7119273507f453b4bb287f7ff69010
Rajkumar Ramesh <rajkumarramesh.251998@gmail.com>
2019-10-24 16:03:34 +0530

    integer and square value

---
+++ square.rb
@@ -0,0 +1,17 @@
+ num1=25
+ num2=16
+ num3=100
+ num4=5
+ puts Integer.sqrt(num1)
+ puts Integer.sqrt(num2)
+ puts Integer.sqrt(num3)
+ puts Integer.sqrt(num4)
+
+puts "Please enter the number:"
+n = gets
+
+n = n.chop
+
+n = n.to_i
+
+p Integer.sqrt(n)
