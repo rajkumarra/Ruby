4c9eabd58bf8c842df7d343a68db31a9acf44b60
Rajkumar Ramesh <rajkumarramesh.251998@gmail.com>
2020-03-18 15:12:54 +0530

    vowelwords

---
+++ vowel.rb
@@ -0,0 +1,34 @@
+#class S
+#   s = "soundar"
+#   vowels = "aeiou"
+  # puts (s.split(vowels))
+ #  def remvowel(string)
+#    string.delete 'aeiou'
+    # end
+   #end
+#    s = "soundar"
+# vowels = "aeiou"
+# puts (s.chars - vowels.chars).join
+
+
+
+# 1
+s = 'soundar'.split('') 
+s = ['s', 'o', 'u', 'n', 'd', 'a', 'r']
+
+
+
+s.each do |c|
+  if c == 'a'
+    puts c
+  elsif c == 'e'
+    puts c
+  elsif c == 'i'
+    puts c
+  elsif c == 'o'
+    puts c
+  elsif c == 'u'
+    puts c
+  end
+end
+
